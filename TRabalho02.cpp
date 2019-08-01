67f33159f5c1bab0f2ca0b00122d715fa6efbb77
Paulao-DS <53355519+Paulao-DS@users.noreply.github.com>
2019-08-01 20:48:58 -0300

    Add files via upload

---
+++ TRabalho02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int i=48;
+	while (i<100) {
+	
+	i=i+2;
+	cout<<i<<endl;}
+	system ("pause");
+	
+}
---
+++ TRabalho03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int i=11;
+	while (i>1) {
+	
+	i=i-1;
+	cout<<i<<endl;}
+	cout<<"fogo"<<endl;
+	system ("pause");
+	
+}
